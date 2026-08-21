3da5393878ecb8a07dc38ecf8a9cccd92ebec070
Aleandro Lima <aleandro.lima@faex.edu.br>
2026-08-21 14:21:54 -0300

    chore: iniciar projeto

---
+++ src/main/java/br/edu/faex/Main.java
@@ -0,0 +1,10 @@
+package br.edu.faex;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        System.out.println("Sistema Acadêmico");
+        System.out.println("Projeto POO II");
+    }
+}
