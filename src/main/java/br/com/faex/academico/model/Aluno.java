f26889c46d58e27e5b6104ec0e6ee7b0394128ec
Aleandro Lima <aleandro.lima@faex.edu.br>
2026-08-21 21:45:20 -0300

    feat: atualização Classe Aluno

--- src/main/java/br/com/faex/academico/model/Aluno.java
+++ src/main/java/br/com/faex/academico/model/Aluno.java
@@ -1,4 +1,46 @@
 package br.com.faex.academico.model;
 
 public class Aluno {
+        private int id;
+        private boolean ativo;
+        private String email;
+        private String nome;
+
+    public Aluno() {
+
+    }
+
+    public Aluno(String email, String nome) {
+        this.email = email;
+        this.nome = nome;
+        this.ativo = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
