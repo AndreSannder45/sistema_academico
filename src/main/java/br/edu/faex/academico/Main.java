abe9d5a1dde260f7de9544b6dec64c5b5f67feea
Aleandro Lima <aleandro.lima@faex.edu.br>
2026-08-28 15:36:06 -0300

    test: testando model Aluno

--- src/main/java/br/edu/faex/academico/Main.java
+++ src/main/java/br/edu/faex/academico/Main.java
@@ -1,17 +1,12 @@
 package br.edu.faex.academico;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.edu.faex.academico.model.Aluno;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        Aluno aluno = new Aluno("Aleandro Ribeiro de Lima", "aleandro.lima@faex.edu.br");
+        System.out.println("Nome: " + aluno.getNome());
+        System.out.println("E-mail: " + aluno.getEmail());
+        System.out.println("Ativo: " + aluno.isAtivo());
     }
 }
